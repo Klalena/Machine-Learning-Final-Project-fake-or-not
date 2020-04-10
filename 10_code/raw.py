d78dbe064284f4057b2febba5e82420b66fafd4d
jdavidmartinezg <20741918+jdavidmartinezg@users.noreply.github.com>
2020-04-10 11:27:34 -0400

    updated packages

--- 10_code/raw.py
+++ 10_code/raw.py
@@ -4,21 +4,29 @@ Created on Thu Apr  9 16:53:46 2020
 
 @author: jdavi
 """
-
+# Basics
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import seaborn as sns
 
-import tensorflow as tf
+# Image processing and ELA conversion
+from PIL import Image
+import os
+from pylab import *
+import re
+from PIL import Image, ImageChops, ImageEnhance
 
-np.random.seed(2)
-
+# General ML models
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 import itertools
+from pandas_ml import ConfusionMatrix
 
+
+# CNN using my laptop GPU
+import tensorflow as tf
 from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
@@ -26,14 +34,10 @@ from keras.optimizers import RMSprop
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ReduceLROnPlateau, EarlyStopping
 
-sns.set(style='white', context='notebook', palette='deep')
+sns.set(style='white', context='paper', palette='deep')
+np.random.seed(2)
 
 
-from PIL import Image
-import os
-from pylab import *
-import re
-from PIL import Image, ImageChops, ImageEnhance
 
 def get_imlist(path):
     return [os.path.join(path,f) for f in os.listdir(path) if f.endswith('.jpg') or f.endswith('.png')]
@@ -43,21 +47,21 @@ def convert_to_ela_image(path, quality):
     filename = path
     resaved_filename = filename.split('.')[0] + '.resaved.jpg'
     ELA_filename = filename.split('.')[0] + '.ela.png'
-    
+
     im = Image.open(filename).convert('RGB')
     im.save(resaved_filename, 'JPEG', quality=quality)
     resaved_im = Image.open(resaved_filename)
-    
+
     ela_im = ImageChops.difference(im, resaved_im)
-    
+
     extrema = ela_im.getextrema()
     max_diff = max([ex[1] for ex in extrema])
     if max_diff == 0:
         max_diff = 1
     scale = 255.0 / max_diff
-    
+
     ela_im = ImageEnhance.Brightness(ela_im).enhance(scale)
-    
+
     return ela_im
 
 paths = get_imlist('D:/ML final project/PS_Battles/dataset/')
@@ -84,8 +88,8 @@ for index, row in dataset.iterrows():
         print("Image {} dropped. Could not be processed.".format(re.sub('D:/ML final project/PS_Battles/dataset/', '', dataset['file_name'][index])))
         pass
 
-    if index % 500 == 0:
-        
+    if index % 1000 == 0:
+
         print(index, "images parsed")
 
 dataset_correct = dataset[dataset.index.isin(new_index)]
@@ -102,25 +106,38 @@ Y = tf.keras.utils.to_categorical(Y, 2)
 
 # np.save("D:/ML final project/PS_Battles/ps_battles_ela_Y.npy", Y)
 
+# dataset_correct.to_csv('D:/ML final project/PS_Battles/dataset_correct.csv')
 
 
-# np.load("D:/ML final project/PS_Battles/ps_battles_ela_X.npy")
-# np.load("D:/ML final project/PS_Battles/ps_battles_ela_Y.npy")
+'''
+*******************************************************************************
+*******************************************************************************
+********************  LOAD .npy FILES AT THIS POINT  **************************
+*******************************************************************************
+*******************************************************************************
+'''
 
+
+'''
+X = np.load("D:/ML final project/PS_Battles/ps_battles_ela_X.npy")
+Y = np.load("D:/ML final project/PS_Battles/ps_battles_ela_Y.npy")
+dataset_correct = pd.read_csv("D:/ML final project/PS_Battles/dataset_correct.csv")
+'''
 
 def plot_image(img_num):
-    
+
     file_name = dataset_correct['file_name'][img_num]
     img_name = re.sub('D:/ML final project/PS_Battles/dataset/', '', file_name)
-    
+
     plt.imshow(X[img_num], interpolation='nearest')
     plt.title("ELA version of image {}".format(img_name))
     plt.show()
     return img_name
-    
-im_name = plot_image(7000)   
 
-Image.open('D:/ML final project/PS_Battles/dataset/'+im_name) 
+# Cheerleaders fake image
+im_name = plot_image(2220)
+
+Image.open('D:/ML final project/PS_Battles/dataset/'+im_name)
 
 
 
@@ -136,16 +153,20 @@ X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size = 0.2, random_
 
 tf.keras.backend.clear_session()
 
-del model
-    
-    
+
+try:
+    del model
+except:
+    print("No model in memory")
+
+
 
 
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(filters = 32, kernel_size = (5,5),padding = 'valid', 
+    tf.keras.layers.Conv2D(filters = 32, kernel_size = (5,5),padding = 'valid',
                      activation ='relu', input_shape = (128,128,3)),
-    tf.keras.layers.Conv2D(filters = 32, kernel_size = (5,5),padding = 'valid', 
+    tf.keras.layers.Conv2D(filters = 32, kernel_size = (5,5),padding = 'valid',
                      activation ='relu'),
     tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
     tf.keras.layers.Dropout(0.25),
@@ -163,20 +184,32 @@ optimizer = tf.keras.optimizers.RMSprop(lr=0.0005, rho=0.9, epsilon=1e-08, decay
 model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
 
 
+
+
+# I test two stopping criterias:
+
 early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy',
                               min_delta=0,
                               patience=2,
                               verbose=0, mode='auto')
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('acc')>0.999):
+      print("\nReached 99.9% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+callbacks = myCallback()
+
+
 
 
 epochs = 30
 batch_size = 100
 
 
-
-
-history = model.fit(X_train, Y_train, batch_size = batch_size, epochs = epochs, 
-          validation_data = (X_val, Y_val), callbacks=[early_stopping])
+history = model.fit(X_train, Y_train, batch_size = batch_size, epochs = epochs,
+          validation_data = (X_val, Y_val), callbacks=[callbacks])
 
 
 
@@ -184,7 +217,7 @@ history = model.fit(X_train, Y_train, batch_size = batch_size, epochs = epochs,
 
 
 
-# Plot the loss and accuracy curves for training and validation 
+# Plot the loss and accuracy curves for training and validation
 
 fig, ax = plt.subplots(1,2, figsize=(15,5))
 ax[0].plot(history.history['loss'], color='b', label="Training loss")
@@ -233,18 +266,17 @@ def plot_confusion_matrix(cm, classes,
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
-    
-    
+
+
 # Predict the values from the validation dataset
 Y_pred = model.predict(X_val)
-# Convert predictions classes to one hot vectors 
-Y_pred_classes = np.argmax(Y_pred,axis = 1) 
+# Convert predictions classes to one hot vectors
+Y_pred_classes = np.argmax(Y_pred,axis = 1)
 # Convert validation observations to one hot vectors
-Y_true = np.argmax(Y_val,axis = 1) 
+Y_true = np.argmax(Y_val,axis = 1)
 # compute the confusion matrix
-confusion_mtx = confusion_matrix(Y_true, Y_pred_classes) 
+confusion_mtx = confusion_matrix(Y_true, Y_pred_classes)
 # plot the confusion matrix
 plot_confusion_matrix(confusion_mtx, classes = range(2))
 
 # ~70% Accuracy
-
